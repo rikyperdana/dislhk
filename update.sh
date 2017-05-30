e7e58e0924b4ad5fb98feff576e33edb1fbe0e0f
Riky Perdana <perdanariky@gmail.com>
2017-05-31 01:15:56 +0700

    fix update cron

--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 # Copy your geojson files to ./public/maps
 # Run this shell with ./update.sh
 
-for i in $(ls ./public/maps); do
-    sed --in-place '/peta/d' ./view.jade
-    sed --in-place '/onRendered/d' ./client.coffee
-    sed --in-place '/push/d' ./array.coffee
-done
+sed --in-place '/peta/d' ./view.jade
+sed --in-place '/onRendered/d' ./client.coffee
+sed --in-place '/push/d' ./array.coffee
 
 for i in $(ls ./public/maps); do
     echo 'template(name="'"${i%.*}"'"): #peta' >> view.jade
