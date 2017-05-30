150389238fca72f689b3d9bd63f38f9906961e21
Riky Perdana <perdanariky@gmail.com>
2017-05-31 01:05:16 +0700

    fix bash cron

---
+++ update.sh
@@ -0,0 +1,11 @@
+for i in $(ls ./public/maps); do
+    sed --in-place '/peta/d' ./view.jade
+    sed --in-place '/onRendered/d' ./client.coffee
+    sed --in-place '/push/d' ./array.coffee
+done
+
+for i in $(ls ./public/maps); do
+    echo 'template(name="'"${i%.*}"'"): #peta' >> view.jade
+    echo '    Template.'${i%.*}'.onRendered -> makeMap "'"${i%.*}"'", "F_Prbhn"' >> client.coffee
+    echo 'petas.push "'"${i%.*}"'"' >> array.coffee
+done
