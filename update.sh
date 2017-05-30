6a1f248badb5aa6d871d92cda4822546d65d77af
Riky Perdana <perdanariky@gmail.com>
2017-05-31 05:56:02 +0700

    fix update generator

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 # Copy your geojson files to ./public/maps
 # Run this shell with ./update.sh
 
-sed --in-place '/peta/d' ./view.jade
-sed --in-place '/onRendered/d' ./client.coffee
-sed --in-place '/push/d' ./array.coffee
+sed --in-place '/peta/d' ./generated/view.jade
+sed --in-place '/onRendered/d' ./generated/client.coffee
+sed --in-place '/push/d' ./generated/array.coffee
 
 for i in $(ls ./public/maps); do
-    echo 'template(name="'"${i%.*}"'"): #peta' >> view.jade
-    echo '    Template.'${i%.*}'.onRendered -> makeMap "'"${i%.*}"'", "F_Prbhn"' >> client.coffee
-    echo 'petas.push "'"${i%.*}"'"' >> array.coffee
+    echo 'template(name="'"${i%.*}"'"): #peta' >> ./generated/view.jade
+    echo '    Template.'${i%.*}'.onRendered -> makeMap "'"${i%.*}"'", "F_Prbhn"' >> ./generated/client.coffee
+    echo 'petas.push "'"${i%.*}"'"' >> ./generated/array.coffee
 done
